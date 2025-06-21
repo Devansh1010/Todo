cdecb29c0a77781c3c7f9f5519f1442d5170720f
Devansh Prajapati <98266228+Devansh1010@users.noreply.github.com>
2025-06-21 15:20:00 +0530

    Add register route. register user with bcrypt hash password

---
+++ src/api/register/route.ts
@@ -0,0 +1,43 @@
+import User from "@/models/User.model";
+import bcrypt from "bcryptjs";
+import { ApiResponce } from "../../../types/ApiResponce";
+
+export async function POST(request: Request): Promise<ApiResponce> {
+    try {
+        const { username, email, password } = await request.json();
+
+        // find user
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return {
+                success: false,
+                message: "User already exists",
+            };
+        }
+
+        // hashPassword
+        const pwHash = await bcrypt.hash(password, 10)
+
+        //create User
+        const user = await User.create({
+            username,
+            email,
+            password
+        })
+
+
+        return {
+            success: true,
+            message: "User Registerd Successfully",
+        };
+
+
+
+    } catch (error) {
+        console.log("Error registering user", error)
+        return {
+            success: false,
+            message: "Error generated while registering user",  
+        };
+    }
+}

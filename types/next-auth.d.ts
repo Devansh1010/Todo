8d3b46d563def21cfda9769d1abdc2b2debb3d89
Devansh Prajapati <98266228+Devansh1010@users.noreply.github.com>
2025-06-22 10:48:07 +0530

    dclare module User, Session and JWT for better access of user data without chocking the database

---
+++ types/next-auth.d.ts
@@ -0,0 +1,27 @@
+import 'next-auth'
+import { DefaultSession } from 'next-auth'
+
+declare module "next-auth" {
+    interface User {
+        _id?: string,
+        username?: string,
+        email?: string
+        role?: string,
+    }
+
+    interface Session {
+        user: {
+            _id?: string,
+            username?: string,
+            email?: string
+            role?: string
+        } & DefaultSession["user"]
+    }
+
+    interface JWT {
+        _id?: string,
+        username?: string,
+        email?: string
+        role?: string
+    }
+}

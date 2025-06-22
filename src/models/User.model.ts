0977a0eaf3ed2ca501133629cb80ad2f6649942b
Devansh Prajapati <98266228+Devansh1010@users.noreply.github.com>
2025-06-22 21:06:35 +0530

    Update the User-model to solve the TS error. (minior changes not necessary to change it work completely fine befor this also)

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,68 +1,75 @@
-import mongoose, { Schema, Document } from 'mongoose'
-import { UserRole } from '@/models/UserRole'
-import { USER_ROLES } from '@/models/UserRole'; 
+import mongoose, { Schema, Document } from 'mongoose';
+import { UserRole } from '@/models/UserRole';
+import { USER_ROLES } from '@/models/UserRole';
 
 export interface User extends Document {
-
-    username: string,
-    email: string,
-    password: string,
-    profilePeacture: string,
-    membership: [{
-        workspaceId: mongoose.Types.ObjectId,
-        role: UserRole,
-        teamsId: [mongoose.Types.ObjectId],
-        joinAt: Date
-    }]
+  username: string;
+  email: string;
+  password: string;
+  profilePicture?: string;
+  membership: Array<{
+    workspaceId: mongoose.Types.ObjectId;
+    teamsId: mongoose.Types.ObjectId[];
+    role: UserRole;
+    joinAt: Date;
+  }>;
 }
 
-const userSchema = new Schema<User>({
-
+const userSchema = new Schema<User>(
+  {
     username: {
-        type: String,
-        required: [true, "Username is Required"],
+      type: String,
+      required: [true, 'Username is required'],
     },
 
     email: {
-        type: String,
-        required: [true, "Email is Required"],
-        unique: true,
-        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Please Enter valid email"]
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        'Please enter a valid email',
+      ],
     },
-    password: {
-        type: String,
-        required: true
-    },
-    profilePeacture: {
-        type: String,
-    },
-    membership: [{
-        workspaceId: {
-            type: Schema.Types.ObjectId,
-            ref: 'Workspace'
 
+    password: {
+      type: String,
+      required: true,
+    },
+
+    profilePicture: {
+      type: String,
+    },
+
+    membership: [
+      {
+        workspaceId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Workspace',
         },
         role: {
-            type: String,
-            enum: USER_ROLES,
-            required: true,
-            default: 'member'
+          type: String,
+          enum: USER_ROLES,
+          required: true,
+          default: 'member',
         },
-        teamsId: [{
+        teamsId: [
+          {
             type: Schema.Types.ObjectId,
-            ref: 'Teams'
-        }],
-
+            ref: 'Teams',
+          },
+        ],
         joinAt: {
-            type: Date,
-            default: Date.now
-        }
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-    }]
 
-}, { timestamps: true })
+const UserModel = mongoose.models.User || mongoose.model<User>('User', userSchema);
 
-
-const UserModel = (mongoose.models.User as mongoose.Model<User>) || (mongoose.model<User>('User', userSchema))
-
-export default UserModel
+export default UserModel;

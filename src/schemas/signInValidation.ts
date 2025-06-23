de67f38f72338d3fd8f722d401f62918994e6638
Devansh Prajapati <98266228+Devansh1010@users.noreply.github.com>
2025-06-23 13:23:04 +0530

    Minior changes on the signInValidation

--- src/schemas/signInValidation.ts
+++ src/schemas/signInValidation.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod'
-import { usernameValidation } from './signUpValidation'
 
 export const signInValidation = z.object({
-    username: z.string().min(2, "Username must be at least 2 characters long"),
+    identifier: z.string().min(2, "Username must be at least 2 characters long"),
     password: z.string().min(6),
 })

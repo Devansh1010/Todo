d6986a535aa99d65f4bdb0e254df7bc12c5b3c08
Devansh Prajapati <98266228+Devansh1010@users.noreply.github.com>
2025-06-26 02:11:05 +0530

    update workspace model and User model littlebit

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -3,6 +3,7 @@ import { UserRole } from '@/models/UserRole';
 import { USER_ROLES } from '@/models/UserRole';
 
 export interface Membership {
+  userId: mongoose.Types.ObjectId
   workspaceId: mongoose.Types.ObjectId;
   teamsId: mongoose.Types.ObjectId[];
   role: UserRole;
@@ -14,61 +15,78 @@ export interface User extends Document {
   email: string;
   password: string;
   profilePicture?: string;
+  workspaceId: mongoose.Types.ObjectId;
+  isAccepted: boolean;
   membership: Membership[]
 }
 
 const UserSchema: Schema<User> = new Schema({
-    username: {
-      type: String,
-      required: [true, 'Username is required'],
-      trim: true,
-      unique: true 
-    },
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    trim: true,
+    unique: true
+  },
 
-    email: {
-      type: String,
-      required: [true, 'Email is required'],
-      unique: true,
-      match: [
-        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-        'Please enter a valid email',
-      ],
-    },
-
-    password: {
-      type: String,
-      required: true,
-    },
-
-    profilePicture: {
-      type: String,
-    },
-
-    membership: [
-      {
-        workspaceId: {
-          type: Schema.Types.ObjectId,
-          ref: 'Workspace',
-        },
-        role: {
-          type: String,
-          enum: USER_ROLES,
-          required: true,
-          default: 'member',
-        },
-        teamsId: [
-          {
-            type: Schema.Types.ObjectId,
-            ref: 'Teams',
-          },
-        ],
-        joinAt: {
-          type: Date,
-          default: Date.now,
-        },
-      },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      'Please enter a valid email',
     ],
   },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  profilePicture: {
+    type: String,
+  },
+
+  workspaceId: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Workspace',
+  }],
+
+  membership: [
+    {
+      userId: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+      },
+      workspaceId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Workspace',
+      },
+      role: {
+        type: String,
+        enum: USER_ROLES,
+        required: true,
+        default: 'member',
+      },
+      teamsId: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Teams',
+        },
+      ],
+
+      isAccepted: {
+        type: Boolean,
+        default: false
+      },
+
+      joinAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+},
   { timestamps: true }
 );
 

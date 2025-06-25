e31700a87dd3a7fcbf4ce7e97af94ca139eecf66
Devansh Prajapati <98266228+Devansh1010@users.noreply.github.com>
2025-06-26 02:08:01 +0530

    add updateAge so jwt updated if user signin in 1 day

--- src/auth.ts
+++ src/auth.ts
@@ -92,6 +92,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   session: {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60,
   },
 
   secret: process.env.AUTH_SECRET,
